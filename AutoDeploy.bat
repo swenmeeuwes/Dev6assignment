c42084d2ced06f1ae71bedc35e4cc9e2ba84eb1a
swenmeeuwes <swenmeeu@gmail.com>
2015-12-13 20:03:40 +0100

    Commit from AutoDeploy.bat on 13-12-2015 20_03_40

--- AutoDeploy.bat
+++ AutoDeploy.bat
@@ -10,7 +10,7 @@ SET solution_location="HTTPrequester.sln"
 SET nuget_location="%~dp0\"
 SET mstest_location="D:\MicrosoftVisualStudio\Common7\IDE\"
 SET git_location="C:\Program Files (x86)\Git\bin\"
-SET artifacts_dir="..\artifacts"
+SET artifacts_dir="..\Artifacts"
 SET dependencies=Newtonsoft.Json Moq
 
 :: VARIABLES
@@ -27,9 +27,10 @@ if "%1"=="" goto:help
 :while
 shift
 if "%0"=="/b" goto:build
-if "%0"=="/c" goto:commit
+if "%0"=="/c" goto:git-commit
 if "%0"=="/h" goto:help
 if "%0"=="/i" goto:install-dependencies
+if "%0"=="/p" goto:git-push
 if "%0"=="/r" goto:run
 if "%0"=="/t" goto:test
 if "%0"=="/u" goto:update-dependencies
@@ -46,10 +47,28 @@ echo Building solution...
 %msbuild_location%MSBuild %solution_location% >> %log%
 goto:while
 
-:commit
-echo Committing...
+:git-commit
+echo Git: Committing...
 %git_location%Git add * >> %log%
 %git_location%Git commit -a -m "Commit from AutoDeploy.bat on %date:~3,10% %time:~0,2%_%time:~3,2%_%time:~6,2%" >> %log%
+goto:while
+
+:git-push
+echo Git: Pushing...
+%git_location%Git push --all origin >> %log%
+goto:while
+
+:help
+echo Usage: AutoDeploy.bat [params]
+echo Available parameters:
+echo /b - Build
+echo /c - Git-Commit
+echo /p - Git-Push
+echo /h - Help
+echo /i - Install-dependencies
+echo /r - Run
+echo /t - Execute Unit Tests
+echo /u - Update-dependencies
 goto:while
 
 :install-dependencies
@@ -57,6 +76,12 @@ echo Installing dependencies...
 (for %%d in (%dependencies%) do (
 	start /b /d %nuget_location% nuget.exe install %%d -o packages|more
 )) >> %log%
+goto:while
+
+:run
+echo Running solution...
+echo Starting application %solution_location:~1,-5% >> %log%
+start /b /d "%solution_location:~1,-5%\bin\Debug\" HTTPrequester.exe "http://145.24.222.160/DataFlowWebservice/api/positions?unitid=357566000058106" >> %log%
 goto:while
 
 :test
@@ -69,23 +94,6 @@ echo Updating dependencies...
 nuget.exe update %solution_location% >> %log%
 goto:while
 
-:run
-echo Running solution...
-echo Starting application %solution_location:~1,-5% >> %log%
-start /b /d "%solution_location:~1,-5%\bin\Debug\" HTTPrequester.exe "http://145.24.222.160/DataFlowWebservice/api/positions?unitid=357566000058106" >> %log%
-goto:while
-
-:help
-echo Usage: AutoDeploy.bat [params]
-echo Available parameters:
-echo /b - Build
-echo /c - Commit
-echo /h - Help
-echo /i - Install-dependencies
-echo /r - Run
-echo /t - Execute Unit Tests
-echo /u - Update-dependencies
-goto:while
 
 :end
 echo Done!
